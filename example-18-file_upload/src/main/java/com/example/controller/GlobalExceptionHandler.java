dffaaaef95189ca0823571eaf18ecd49eef7960d
chenrunlin <chenrunlin516@163.com>
2018-02-26 15:49:06 +0800

    springboot-fileUpload 文件上传

---
+++ example-18-file_upload/src/main/java/com/example/controller/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ * @author runlin.chen
+ * @version 1.0 2018-02-26 15:12
+ **/
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    //https://jira.spring.io/browse/SPR-14651
+    //4.3.5 supports RedirectAttributes redirectAttributes
+    @ExceptionHandler(MultipartException.class)
+    public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("message", "ERROR!：" + e.getCause().getMessage());
+        return "redirect:/uploadStatus";
+    }
+}
